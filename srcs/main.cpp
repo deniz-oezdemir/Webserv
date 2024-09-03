37551c46ee0d54cd54d8033f23add2cd230a150a
migmanu <jmanuelmigoya@gmail.com>
2024-09-03 20:53:11 +0200

    fix: styling that I forgot before

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -14,21 +14,20 @@ int main(int argc, char *argv[])
 		ServerInput input(argc, argv);
 		if (input.hasThisFlag(ServerInput::HELP))
 			std::cout << input.getHelpMessage() << std::endl;
-		if (input.hasThisFlag(ServerInput::V_LITE) ||
-			input.hasThisFlag(ServerInput::V_FULL))
+		if (input.hasThisFlag(ServerInput::V_LITE)
+			|| input.hasThisFlag(ServerInput::V_FULL))
 			std::cout << input.getVersionMessage() << std::endl;
 		ServerConfig config(input.getFilePath());
 		config.parseFile(
 			input.hasThisFlag(ServerInput::TEST),
 			input.hasThisFlag(ServerInput::TEST_PRINT)
 		);
-		if (input.hasThisFlag(ServerInput::TEST) ||
-			input.hasThisFlag(ServerInput::TEST_PRINT))
+		if (input.hasThisFlag(ServerInput::TEST)
+			|| input.hasThisFlag(ServerInput::TEST_PRINT))
 		{
 			if (config.isConfigOK())
-				std::cout << PURPLE "<WebServ> "
-						  << RESET "Test finished: " << GREEN BOLD "Config OK!" RESET
-						  << std::endl;
+				std::cout << PURPLE "<WebServ> " << RESET "Test finished: "
+						  << GREEN BOLD "Config OK!" RESET << std::endl;
 			if (input.hasThisFlag(ServerInput::TEST_PRINT))
 				config.printConfig();
 			return 0;
