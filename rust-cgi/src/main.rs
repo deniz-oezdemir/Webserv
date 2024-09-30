3ce6af6f4c0224f8cd0992a95c397f8fcfbf69b9
Jose Manuel Migoya Bussolotti <jmanuelmigoya@gmai.com>
2024-09-30 20:08:05 +0200

    feat: add basic Rust CGI script for handling input data
    Added a basic Rust CGI script that reads input data based on the
    CONTENT_LENGTH environment variable. This script demonstrates how to
    access environment variables, read from stdin, and handle input data
    correctly.
    
    - Updated .gitignore to exclude the target directory for the Rust CGI
      project.
    - Created a new Cargo.lock file for the Rust CGI project.
    - Implemented main.rs to read CONTENT_LENGTH and input data from stdin.

--- rust-cgi/src/main.rs
+++ rust-cgi/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use std::{env, io::{self, Read}};
+
+fn main() -> io::Result<()> {
+    let vars = std::env::vars();
+    for (key, value) in vars {
+        if key == "CONTENT_LENGTH" {
+            println!("{key}: {value}");
+        }
+    }
+
+    let length: usize = env::var("CONTENT_LENGTH")
+        .unwrap_or_else(|_| "0".to_string())
+        .parse()
+        .expect("Cannot transfor to integer");
+
+    // Create a buffer to hold the input data
+    let mut buffer = vec![0; length];
+
+    // Read the exact number of bytes from stdin
+    io::stdin().read_exact(&mut buffer)?;
+
+    // Convert the buffer to a string and print it
+    let input = String::from_utf8_lossy(&buffer);
+    println!("Input was: {input}");
+
+    Ok(())
 }
