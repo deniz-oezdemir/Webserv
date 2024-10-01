0908eca183d043d4bbf2cfe2a9335a46f5559d7b
Jose Manuel Migoya Bussolotti <jmanuelmigoya@gmai.com>
2024-10-01 10:56:55 +0200

    feat: add basic CGI script to handle image uploads
    Implemented a basic CGI script in Rust to handle image uploads for an
    Instagram clone. The script reads the CONTENT_LENGTH environment
    variable and the image data from stdin, then prints a simple HTTP
    response.
    
    - Added logic to read CONTENT_LENGTH and input data from stdin.
    - Implemented error handling for invalid CONTENT_LENGTH and stdin
      reading errors.
    - Created a basic HTTP response with Content-Type header and HTML body.

--- rust-cgi/src/main.rs
+++ rust-cgi/src/main.rs
@@ -1,4 +1,7 @@
-use std::{env, io::{self, Read}};
+use std::{
+    env,
+    io::{self, Read},
+};
 
 fn main() -> io::Result<()> {
     let vars = std::env::vars();
@@ -8,20 +11,56 @@ fn main() -> io::Result<()> {
         }
     }
 
-    let length: usize = env::var("CONTENT_LENGTH")
-        .unwrap_or_else(|_| "0".to_string())
-        .parse()
-        .expect("Cannot transfor to integer");
+    // Read the CONTENT_LENGTH environment variable and convert it to an integer
+    let length: usize = match env::var("CONTENT_LENGTH") {
+        Ok(val) => match val.parse::<usize>() {
+            Ok(num) => num,
+            Err(_) => {
+                eprintln!("Error: CONTENT_LENGTH is not a valid integer");
+                return Err(io::Error::new(
+                    io::ErrorKind::InvalidInput,
+                    "Invalid CONTENT_LENGTH",
+                ));
+            }
+        },
+        Err(_) => {
+            eprintln!("Error: CONTENT_LENGTH is not set");
+            return Err(io::Error::new(
+                io::ErrorKind::NotFound,
+                "CONTENT_LENGTH not set",
+            ));
+        }
+    };
 
     // Create a buffer to hold the input data
     let mut buffer = vec![0; length];
 
     // Read the exact number of bytes from stdin
-    io::stdin().read_exact(&mut buffer)?;
+    // Read the exact number of bytes from stdin
+    if let Err(e) = io::stdin().read_exact(&mut buffer) {
+        eprintln!("Error reading stdin: {}", e);
+        return Err(e);
+    }
 
     // Convert the buffer to a string and print it
     let input = String::from_utf8_lossy(&buffer);
     println!("Input was: {input}");
 
+    // Print HTTP resonse
+    print_response();
+
     Ok(())
+}
+
+fn print_response() {
+    // Print the HTTP header
+    println!("Content-Type: text/html\n");
+
+    println!("<html>");
+    println!("<head><title>CGI Response</title></head>");
+    println!("<body>");
+    println!("<h1>Hello from Rust CGI!</h1>");
+    println!("<p>This is a simple CGI response.</p>");
+    println!("</body>");
+    println!("</html>");
 }
