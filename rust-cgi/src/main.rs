7895264d43aa7a29388e8f1bb77c2b86f943f8df
migmanu <jmanuelmigoya@gmail.com>
2024-10-01 18:54:23 +0200

    feat: add file existence check before deletion
    Added a check to verify if the file exists before attempting to delete
    it. This improvement provides more informative error messages and
    prevents the script from attempting to delete non-existent files.
    
    - Skip the first argument (program name) to avoid deleting the executable.
    - Check if the target file path is provided as the second argument.
    - Use `std::fs::metadata` to check if the file exists.
    - Provide appropriate error messages if the file does not exist.
    - Attempt to delete the file if it exists and handle any errors
      gracefully.

--- rust-cgi/src/main.rs
+++ rust-cgi/src/main.rs
@@ -1,66 +1,34 @@
-use std::{
-    env,
-    io::{self, Read},
-};
+use std::io::{self, Error};
 
 fn main() -> io::Result<()> {
-    let vars = std::env::vars();
-    for (key, value) in vars {
-        if key == "CONTENT_LENGTH" {
-            println!("{key}: {value}");
-        }
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() != 2 {
+        return Err(Error::new(
+            io::ErrorKind::InvalidInput,
+            "Incorrect number of arguments to rust-cgi.",
+        ));
     }
 
-    // Read the CONTENT_LENGTH environment variable and convert it to an integer
-    let length: usize = match env::var("CONTENT_LENGTH") {
-        Ok(val) => match val.parse::<usize>() {
-            Ok(num) => num,
-            Err(_) => {
-                eprintln!("Error: CONTENT_LENGTH is not a valid integer");
-                return Err(io::Error::new(
-                    io::ErrorKind::InvalidInput,
-                    "Invalid CONTENT_LENGTH",
-                ));
-            }
-        },
+    let path = args[1].clone();
+
+    // Check if the file exists
+    if std::fs::metadata(&path).is_err() {
+        eprintln!("File does not exist: {path}");
+        return Err(Error::new(
+            io::ErrorKind::NotFound,
+            "File does not exist.",
+        ));
+    }
+
+    match std::fs::remove_file(&path) {
+        Ok(_) => {
+            eprintln!("File deleted: {path}")
+        }
         Err(_) => {
-            eprintln!("Error: CONTENT_LENGTH is not set");
-            return Err(io::Error::new(
-                io::ErrorKind::NotFound,
-                "CONTENT_LENGTH not set",
-            ));
+            eprintln!("File {path} could not be deleted")
         }
     };
 
-    // Create a buffer to hold the input data
-    let mut buffer = vec![0; length];
-
-    // Read the exact number of bytes from stdin
-    // Read the exact number of bytes from stdin
-    if let Err(e) = io::stdin().read_exact(&mut buffer) {
-        eprintln!("Error reading stdin: {}", e);
-        return Err(e);
-    }
-
-    // Convert the buffer to a string and print it
-    let input = String::from_utf8_lossy(&buffer);
-    println!("Input was: {input}");
-
-    // Print HTTP resonse
-    print_response();
-
     Ok(())
-}
-
-fn print_response() {
-    // Print the HTTP header
-    println!("Content-Type: text/html\n");
-
-    println!("<html>");
-    println!("<head><title>CGI Response</title></head>");
-    println!("<body>");
-    println!("<h1>Hello from Rust CGI!</h1>");
-    println!("<p>This is a simple CGI response.</p>");
-    println!("</body>");
-    println!("</html>");
 }
